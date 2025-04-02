6652df949c951af5e98896d899b8feef00300627
greendangerscratch <felixhorten65@gmail.com>
2025-04-02 17:43:51 +1100

    GAS????????????

---
+++ gasoline.js
@@ -0,0 +1,9 @@
+elements.gasoline = {
+    behavior: behaviors.LIQUID,
+    category: "liquids",
+    state: "liquid",
+    density: 1,
+    color: ["#005F6A"],
+    breakInto: "water",
+    breakIntoColor: "#005F6A",
+    reactions: {},
